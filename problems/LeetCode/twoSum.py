fa6cfef1a5eb80fa60fdd31985da49f0700b574a
Toky <tokybear2003@gmail.com>
2026-09-05 19:32:44 +0700

    Update twoSum with more explainations

--- problems/LeetCode/twoSum.py
+++ problems/LeetCode/twoSum.py
@@ -3,18 +3,22 @@ class Solution(object):
         numMap = {}     # Create an empty dictionry 
         n = len(nums)   # Define length of list
 
-        # Build the hash table 
-        for i in range(n):  # i sequence start from 0 -> given length of n
+        # Build the hash table - assign each key with position of value 
+        for i in range(n):  
             numMap[nums[i]] = i
 
-        # Find the complement: x + y = Target. For x = nums[i] and y = complement
+        # Find the complement, if x + y = Target
+        # x: i and y: compliment -> Target = i + compliment 
+        # => compliment(hash_value) = Target - i        
         for i in range(n):
             complement = target - nums[i]
             if complement in numMap and numMap[complement] != i:
                 return [i, numMap[complement]]
         return []
 
+# Test the function
 sol = Solution()
 lst = [3,4,6,7,11]
 target = 13
+
 print(sol.twoSum(lst, target))
