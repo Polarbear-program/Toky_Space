72c2a618f91060326eb0b993e1f2ff356906a71c
Toky <tokybear2003@gmail.com>
2026-09-05 16:21:55 +0700

    add self_instance_function.py update README

---
+++ problems/LeetCode/self_Instance_function.py
@@ -0,0 +1,9 @@
+# One of the OOP way to call and modify a self-class instances
+
+class Solution:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        pass
+
+# As far as I know, this self-calling parameter 
+# is to replace the __init__ function
+# We can still use instance inside the class itself
